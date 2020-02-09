0487eb69806743c5bde0d84260bbf577d5ded78a
pavel <pavel.vjalicin@softar.uk>
2020-02-09 07:51:00 +0000

    Ignoring dist folder

--- dist/dev-build.js
+++
@@ -1,9 +0,0 @@
-var Server = require("./server.js").default;
-var webpack = require("webpack");
-var webpackConfig = require("./webpack.config.js");
-webpack(webpackConfig, function (err, stats) {
-    server.sendMessageToWS("refresh");
-});
-var server = Server(3000);
-server.start();
-//# sourceMappingURL=dev-build.js.map
