f1e30d660ad88bf41a3cef6b5fde2ff86128fce7
pavel <pavel.vjalicin@softar.uk>
2020-02-09 12:42:33 +0000

    Implemented collision detection with different player scale settings.

--- src/javascripts/player.ts
+++ src/javascripts/player.ts
@@ -4,13 +4,14 @@ import { EntityManager } from "./engine/entityManager";
 class Player extends Box {
     private inputs = EntityManager.inputs
     private ySpeed = 0;
-    private gravity = 0.05;
+    private gravity = 0.01;
     private grounded = true;
-    private speed = 0.1;
+    private speed = 0.10;
     private sizeX;
     private sizeY;
-    private tileSizeX = 1;
-    private tileSizeY = 1;
+    private jumpForce = 0.3
+    private tileSizeX = 2;
+    private tileSizeY = 2;
     private map;
     constructor(x,y,z,sizeX,sizeY,sizeZ,map) {
         super(x,y,z,sizeX,sizeY,sizeZ)
@@ -39,7 +40,7 @@ class Player extends Box {
         }
 
         if(this.grounded && this.inputs.UP) {
-            this.ySpeed = 1;
+            this.ySpeed = this.jumpForce;
             this.grounded = false;
         }
 
@@ -52,15 +53,19 @@ class Player extends Box {
         let xTile = location[0]
         let yTile = location[1]
         
-        let downTileOccupied =  this.isDownTileOccupied()
+        let floatLoc = this.getTileLocationFloat()
+        let x =  floatLoc[0]
+        let y =  floatLoc[1]
+        
+        let downTileOccupied =  this.isDirectionOccupied(4)
 
         if(downTileOccupied) {
-            if(this.ySpeed<0) {
-                let tileOccupation = (this.sizeY * 0.5)
-                if(tileOccupation + moveY + (this.y % 1) < 0.5) {
+            if(moveY<0) {
+                let bottomLocation = 2*yTile - (this.tileSizeY - (this.sizeY*2))/2
+                if(this.y + moveY < bottomLocation) {
                     this.grounded = true
                     moveY = 0
-                    this.y = (yTile*2) - 1 + this.sizeY
+                    this.y = bottomLocation
                 } 
             }
             
@@ -68,33 +73,106 @@ class Player extends Box {
             this.grounded = false
         }
 
+        let rightIsOccupied = this.isDirectionOccupied(3)
+
+        if(rightIsOccupied) {
+            if(moveX>0) {
+                let rightLocation = 2*xTile -4 + (this.tileSizeX - (this.sizeX*2))/2 ;
+                
+                if(this.x + moveX > rightLocation) {
+                    moveX = 0
+                    this.x = rightLocation
+                }
+            }
+        }
+
+        let leftIsOccupied = this.isDirectionOccupied(1)
+
+        if(leftIsOccupied) {
+            if(moveX<0) {
+                let leftLocation = 2*xTile -4 - (this.tileSizeX - (this.sizeX*2))/2 ;
+                
+                if(this.x + moveX < leftLocation) {
+                    moveX = 0
+                    this.x = leftLocation
+                }
+            }
+        }
+
         this.x += moveX;
         this.y += moveY
     }
-
-    isDownTileOccupied() {
+    //1 = LEFT, 2 = UP, 3 = RIGHT, 4 = DOWN
+    isDirectionOccupied(direction) {
         let location = this.getTileLocation()
         let xTile = location[0]
         let yTile = location[1]
         
-        let downTile = this.getMapTile(xTile,yTile+1) != 0  
-        let relativePosition = ((this.x*0.5+3)-xTile) - 1
-        let downLeftTile = relativePosition < -this.sizeX && this.getMapTile(xTile-1,yTile+1) != 0  
-        let downRightTile = relativePosition > this.sizeX && this.getMapTile(xTile+1,yTile+1) != 0  
-        console.log(relativePosition)
-        return downLeftTile || downTile  || downRightTile  
+        let floatLoc = this.getTileLocationFloat()
+        let x =  floatLoc[0]
+        let y =  floatLoc[1]
+
+        if(direction == 1) {
+            xTile -= 1
+        } else if(direction == 2){
+            yTile += 1
+        } else if(direction == 3) {
+            xTile += 1
+        } else if(direction == 4) {
+            yTile -= 1
+        }
+        
+        
+        let directionTile = this.getMapTile(xTile,yTile) != 0  
+    
+
+        let relativePosition;
+        let adjecentIndexes;
+        let size;
+        if(direction == 2 || direction ==  4) {
+            
+            size = (1-this.sizeX)/2
+            relativePosition = (-x+xTile )
+            adjecentIndexes = [[1,-1],[0,0]]
+        } else if(direction == 1 || direction ==  3) {
+            size = (1-this.sizeY)/2
+            relativePosition = (-y+yTile )
+            adjecentIndexes = [[0,0],[-1,1]]
+        }
+        
+        
+
+        let downLeftTile = this.getMapTile(xTile+adjecentIndexes[0][1],yTile+adjecentIndexes[1][1]) != 0 && (relativePosition - size) > 0
+        let downRightTile = this.getMapTile(xTile+adjecentIndexes[0][0],yTile+adjecentIndexes[1][0]) != 0 && (relativePosition + size) < 0
+        if(direction == 3) {
+            
+            console.log(this.map)
+            console.log(adjecentIndexes)
+            console.log( this.getMapTile(xTile+adjecentIndexes[0][1],yTile+adjecentIndexes[1][1]))
+            console.log( this.getMapTile(xTile+adjecentIndexes[0][0],yTile+adjecentIndexes[1][0]))
+            console.log(xTile,yTile)
+            console.log(downLeftTile, directionTile  , downRightTile)
+            
+        }
+        
+
+        return downLeftTile || directionTile  || downRightTile  
     }
 
     getMapTile(x,y) {
         let tile = 0;
-        if(this.map[-y+2] != undefined && this.map[-y+2][x] != undefined) {
-            tile = this.map[-y+2][x] 
+        let yy = -y
+        if(this.map[yy] != undefined && this.map[yy][x] != undefined) {
+            tile = this.map[yy][x] 
         }
         return tile
     }
-
+    getTileLocationFloat() {
+        return [(this.x*0.5)+2,(this.y*0.5)]
+    }
     getTileLocation() {
-        return [Math.round(this.x*0.5)+2,Math.round(this.y*0.5)]
+        let loc = this.getTileLocationFloat()
+        return [Math.round(loc[0]),Math.round(loc[1])]
     }
 }
 
